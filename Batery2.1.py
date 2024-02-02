b70a3dc1b319f5e21d2c85619039c840e7b75d96
ericcarboneras <158280830+ericcarboneras@users.noreply.github.com>
2024-02-02 13:51:11 +0100

    Create Batery2.1.py

---
+++ Batery2.1.py
@@ -0,0 +1,5 @@
+nom_usuari = input("introdueix el teu nom: ")
+if nom_usuari.lower() == "amilcar" or nom_usuari.lower() == "sila":
+    print(f"Benvingut, {nom_usuari}!")
+else:
+    print("L'accés no autoritzat, només es dóna la benvinguda a Amilcar i Sila.")
