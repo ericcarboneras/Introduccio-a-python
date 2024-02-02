a68675f5e2707b0252dbc4a1099683265c0482ae
ericcarboneras <158280830+ericcarboneras@users.noreply.github.com>
2024-02-02 13:53:20 +0100

    Create Batery2.2.py

---
+++ Batery2.2.py
@@ -0,0 +1,7 @@
+numero = float(input("introdueix un número: "))
+if numero > 100:
+    print(f"El número {numero} és major que 100.")
+elif numero < 100:
+    print(f"El número {numero} és menor que 100.")
+else:
+    print("El número és igual a 100.")
