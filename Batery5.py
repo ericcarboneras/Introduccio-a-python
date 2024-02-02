c517119e73649c1619624a2f8e8cde04be2cfe0e
ericcarboneras <158280830+ericcarboneras@users.noreply.github.com>
2024-02-02 13:48:54 +0100

    Create Batery5.py

---
+++ Batery5.py
@@ -0,0 +1,3 @@
+temperatura_celsius = float(input("introdueix la temperatura en graus Celsius: "))
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+print(f"La temperatura en graus Fahrenheit és: {temperatura_fahrenheit:.2f} °F")
