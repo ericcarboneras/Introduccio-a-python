3e866fc27277cd206bc7ea5aef0c5745169f3fc3
ericcarboneras <158280830+ericcarboneras@users.noreply.github.com>
2024-02-15 13:43:12 +0100

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,28 @@
+comunitat = ["Gandalf", "Legolas", "Gimli", "Aragorn", "Merry", "Pippin", "Frodo", "Sam", "Boromir"]
+print(comunitat[0])
+
+print(comunitat[-1])
+
+posicion_media = len(comunitat) // 2
+print(comunitat[posicion_media])
+
+primer_alfabeticamente = min(comunitat, key=len)
+print(primer_alfabeticamente)
+
+darrer_alfabeticament = max(comunitat)
+print(darrer_alfabeticament)
+
+comunitat.remove("Aragorn")
+primer_alfabeticament = min(comunitat)
+print(primer_alfabeticament)
+
+comunitat.sort()
+print(comunitat)
+
+
+
+
+
+
+
+
